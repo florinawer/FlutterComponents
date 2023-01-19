d1eeb6c7b780ba7b1c4103e3759eb4c4c67b1352
Florin Pentelescu <florin.pentelescu@capgemini.com>
2023-01-19 11:32:16 +0100

    Formulario con validaciones y captura de datos

--- flutty_components/lib/screens/inputs_screen.dart
+++ flutty_components/lib/screens/inputs_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../widgets/widgets.dart';
 
 class InputsScreen extends StatelessWidget {
@@ -7,6 +6,15 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //key es para mantener referencia a los widgets
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'user_name': 'Ves',
+      'email': 'Puxi@google.com',
+      'password': 'puxi'
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs y Forms'),
@@ -14,8 +22,57 @@ class InputsScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(
-            children: const [CustomInputField()],
+          child: Form(
+            key: myFormKey,
+            child: Column(
+              children: [
+                //se pasan por parametro los valores y se reciben por constructor
+                CustomInputField(
+                  labelText: "user_name",
+                  hintText: "user_name",
+                  formProperty: 'user_name',
+                  formValues: formValues,
+                ),
+                const SizedBox(height: 30),
+
+                CustomInputField(
+                  labelText: "email", hintText: "email",
+                  //se le pasa por constructor el valor del input para personalizar input
+                  keyBoardType: TextInputType.emailAddress,
+
+                  //en el form type se pone el valor de Mapper
+                  formProperty: 'email', formValues: formValues,
+                ),
+                const SizedBox(height: 30),
+
+                CustomInputField(
+                  labelText: "password",
+                  hintText: "password",
+                  isPassword: true,
+                  formProperty: 'password',
+                  formValues: formValues,
+                ),
+                const SizedBox(height: 30),
+
+                ElevatedButton(
+                  child: const SizedBox(
+                      width: double.infinity,
+                      child: Center(child: Text('Save'))),
+                  onPressed: () {
+                    FocusScope.of(context).requestFocus(FocusNode());
+
+                    //valida el estado y niega el validate
+                    if (!myFormKey.currentState!.validate()) {
+                      print('form no valid');
+                      return;
+                    }
+
+                    //*imprimir valores del form
+                    print(formValues);
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
